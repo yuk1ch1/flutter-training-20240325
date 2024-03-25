2a0f6e869f83a68a08f063049315dedd3778a07f
GitHub Action <action@github.com>
2024-03-25 02:24:45 +0000

    Template cleanup

---
+++ android/app/src/main/kotlin/jp/co/yumemi/flutter_training/MainActivity.kt
@@ -0,0 +1,5 @@
+package jp.co.yumemi.flutter_training
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- tools/cleanup_template/lib/src/file_system.dart
+++
@@ -1,8 +0,0 @@
-import 'package:file/file.dart';
-import 'package:file/local.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'file_system.g.dart';
-
-@Riverpod(dependencies: [])
-FileSystem fileSystem(FileSystemRef ref) => LocalFileSystem();
