114f86f310c4af22cdcd3c5991ba3a364fb4cd23
yuk1ch1 <sma2ww@gmail.com>
2024-04-07 13:17:54 +0900

    fix: StatefulWidgetに変更

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class WeatherScreen extends StatelessWidget {
+class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
 
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
     const margin = SizedBox(height: 80);
