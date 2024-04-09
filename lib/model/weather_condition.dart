172e3333e355052d8e981f38a0479529ac5231d8
yuk1ch1 <sma2ww@gmail.com>
2024-04-09 16:49:54 +0900

    apply review: 例外にメッセージを追加
    -
    デバッグがしやすくするため

--- lib/model/weather_condition.dart
+++ lib/model/weather_condition.dart
@@ -6,7 +6,7 @@ enum WeatherCondition {
   factory WeatherCondition.from(String name) {
     return WeatherCondition.values.singleWhere(
       (element) => element.name == name,
-      orElse: () => throw Exception(),
+      orElse: () => throw Exception('Unexpected weather condition: $name'),
     );
   }
 }
