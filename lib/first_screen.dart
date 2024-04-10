9c948cd9dc0b07872489fdd0b63156121871a5d8
yuk1ch1 <sma2ww@gmail.com>
2024-04-10 12:48:58 +0900

    起動後初めに表示される画面を表示

---
+++ lib/first_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const ColoredBox(color: Colors.green);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_training/first_screen.dart';
 import 'package:flutter_training/ui/weather_screen.dart';
 
 void main() {
@@ -10,8 +11,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: WeatherScreen(),
+    return MaterialApp(
+      routes: {
+        '/': (context) => const FirstScreen(),
+      },
     );
   }
 }
