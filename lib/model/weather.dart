d410407d2073d1e62df2e71e815508407dd6f674
yuk1ch1 <sma2ww@gmail.com>
2024-04-19 19:24:53 +0900

    feat: fetchThrowsWeatherに変更してエラーハンドリング

--- lib/model/weather.dart
+++ lib/model/weather.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_training/model/weather_condition.dart';
+import 'package:flutter_training/model/weather_exception.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 class Weather {
@@ -7,12 +8,24 @@ class Weather {
   final YumemiWeather _client;
 
   WeatherCondition? fetch() {
-    final response = _client.fetchSimpleWeather();
     try {
+      final response = _client.fetchThrowsWeather('Tokyo');
       return WeatherCondition.from(response);
+    } on YumemiWeatherError catch (e) {
+      return switch (e) {
+        YumemiWeatherError.invalidParameter =>
+          throw const InvalidParameter(message: '入力情報に誤りがあります。入力情報をご確認ください'),
+        YumemiWeatherError.unknown =>
+          throw const UnknownWeather(message: '天気情報の取得に失敗しました。時間をおいて再度お試しください'),
+      };
+    } on UndefinedWeather catch (e) {
+      debugPrint(e.toString());
+      throw const UndefinedWeather(message: '天気情報の取得に失敗しました。適切な情報を取得できませんでした');
     } on Exception catch (e) {
       debugPrint(e.toString());
-      return null;
+      throw const UnexpectedException(
+        message: '予期せぬエラーが発生しました。再度時間をおいてお試しください',
+      );
     }
   }
 }
